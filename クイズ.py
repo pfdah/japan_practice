a4cd9863c456a714164756532557b6394998d449
pratik <pratik@192.168.1.19>
2024-07-01 16:14:54 +0545

    added score tracking mechanism.

--- クイズ.py
+++ クイズ.py
@@ -24,15 +24,20 @@ if user_ask.upper() == 'H':
     print('You will practise Hiragana now. You will enter Romaji equivalent of the sounds. Enter "q" in the case you want to quit practice')
     print("===========================")
     usr_ans = ''
+    usr_score = 0
+    usr_attempts = 0
     while not usr_ans == 'q':
         hiragana, romaji = choice(list(hiraganas.items()))
         usr_ans = input(f'Enter Romaji for {hiragana}\n')
         if usr_ans.lower() == 'q':
-            print('Thank you!')
+            print(f'Thank you! You scored {usr_score}/{usr_attempts} in the Hiragana quiz.')
             break
         elif usr_ans.lower() == romaji:
+            usr_attempts += 1
+            usr_score += 1
             print('Correct!')
         else:
+            usr_attempts += 1
             print(f'Incorrect! The correct pronunciation is "{romaji}"')
 
 elif user_ask.upper() == 'K':
@@ -41,11 +46,13 @@ elif user_ask.upper() == 'K':
     print('You will practise Katakana now. You will enter Romaji equivalent of the sounds. Enter "q" in the case you want to quit practice')
     print("===========================")
     usr_ans = ''
+    usr_score = 0
+    usr_attempts = 0
     while not usr_ans == 'q':
         katakana, romaji = choice(list(katakanas.items()))
         usr_ans = input(f'Enter Romaji for {katakana}\n')
         if usr_ans.lower() == 'q':
-            print('Thank you!')
+            print(f'Thank you! You scored {usr_score}/{usr_attempts} in the Hiragana quiz.')
             break
         elif usr_ans.lower() == romaji:
             print('Correct!')
